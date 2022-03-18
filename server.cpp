2dd9a67e1e382da0e45552f38716c2ab139b974e
Adrien DUBOIS <addubois@e2r9p16.clusters.42paris.fr>
2022-03-18 13:09:06 +0100

    first commit

--- server.cpp
+++ server.cpp
@@ -2,15 +2,75 @@
 
 server::server(char * port_number, char * pswd) : _pswd(pswd)
 {
-	_sockfd = socket(AF_INET, SOCK_STREAM, 0); //should have an error gestion
-	_sockaddr.sin_family = AF_INET;
-	_sockaddr.sin_addr.s_addr = INADDR_ANY; // should use get* fcnt
-	_sockaddr.sin_port = htons(atoi(port_number));
-	bind(_sockfd, (struct sockaddr *)(&_sockaddr), sizeof(_sockaddr)); //should have an error gestion
-	listen(_sockfd, 10); // should have an error gestion
+	addrinfo hints, *res, *p; // res = potential adress p = adress iterator;
+	memset(&hints, 0, sizeof(hints));
+	
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
+	int gaddres = getaddrinfo(NULL, port_number, &hints, &res);
+	if(gaddres != 0)
+	{
+		std::cout << gai_strerror(gaddres) << "\n";
+		throw ConstructorException();
+	}
+	for(p = res; p != NULL; p = p->ai_next)
+	{
+		_sockfd = socket(p->ai_family, p->ai_socktype, p->ai_socktype);
+		
+		if(bind(_sockfd, p->ai_addr, p->ai_addrlen) != 0)
+		{
+			std::cout << "error : bind fail";
+			close(_sockfd);
+			continue;
+		}
+		else
+		{
+			if (p->ai_family == AF_INET)
+            {
+                struct sockaddr_in *ipv4 = reinterpret_cast<struct sockaddr_in *>(p->ai_addr);
+               _hostname = std::string(inet_ntoa(ipv4->sin_addr));
+            }
+			else
+			{
+				_hostname = std::string("localhost_ipv6");
+			}
+			std::cout << _hostname << std::endl;
+			    
+            if (listen(_sockfd, 10) != 0)
+            {
+                std::cout << "listen error" << std::endl;
+            }
+            else
+            {  
+                break ;
+            }
+			close(_sockfd);
+		}
+	}
+	freeaddrinfo(res);
+
+	if (p == NULL)
+    {
+        std::cout << "no addr" << std::endl;
+        throw ConstructorException();
+    }
 }
 
+void server::loop()
+{
+	int numsock;
+	int max_fd; = ??;
+	timeval time;
+	time.tv_sec = 60;
+	time.tv_usec= 0;
+	while(true)
+	{
+	numsock = select(max_fd + 1, &_ready_sockets, NULL, NULL, time);
+
+	}
+}
 server::~server()
 {
 	close(_connection);
--- server.hpp
+++ server.hpp
@@ -1,24 +1,38 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <cstdlib>
+#include <cstring>
 #include <iostream> 
 #include <unistd.h>
 #include <cerrno>
 #include <string>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <exception>
 
 class server
 {
 	public:
 		server(char *port_number, char * pswd); // constructor should start evrything until listening
 		~server();
-
+	
+		void loop();
 		//need a loop fcnt with select and acces;
 	private:
+	    struct ConstructorException : std::exception
+        {
+            const char * what()
+            {
+                return "Constructor error\n";
+            }
+        };
 		int _sockfd;
 		char * _pswd;
 		sockaddr_in _sockaddr;
 		int _connection;
 		std::string _welcome_msg;
+		std::string _hostname;
 
 };
